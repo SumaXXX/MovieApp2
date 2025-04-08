9a3f40d8e1b8475646f497d0e0f956631d3cf39b
SumaXXX <kitasolovnik@gmail.com>
2025-04-08 17:44:01 +0300

    Update Card-Content.jsx

--- src/components/Card-Content/Card-Content.jsx
+++ src/components/Card-Content/Card-Content.jsx
@@ -42,7 +42,7 @@ const CardContent = ({
       <img
         className="movie-card--poster"
         alt="poster"
-        src={'src/noImage.png'}
+        src={poster ? `${_apiPosterbase}${poster}` : 'src/noImage.png'}
         // onError={handleError}
       />
       
